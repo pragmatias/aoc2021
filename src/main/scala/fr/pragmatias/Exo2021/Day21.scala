59f71741e5b3ed418806bcf77ad53bcf78647373
pragmatias <gregory.matias@pragmatias.fr>
2022-01-24 19:10:17 +0100

    Day21 p1 & p2

--- src/main/scala/fr/pragmatias/Exo2021/Day21.scala
+++ src/main/scala/fr/pragmatias/Exo2021/Day21.scala
@@ -2,87 +2,123 @@ package fr.pragmatias.Exo2021
 
 import fr.pragmatias.Tools
 
+import scala.annotation.tailrec
 
-class Day20(inputData : String) {
-  
-    val inputList : List[(String)] = new Tools.FileInput(this.inputData).listContent.filterNot(_.trim.isEmpty)
 
-    class Pixel(val char : Char) {
-        def getBinary : String = char match {
-            case '#' => "1"
-            case _ => "0"
-        }
-    }
+class Day21 (inputData : String) {
 
-    class Image(val pixels : IndexedSeq[IndexedSeq[Pixel]], val pixelOutofImage : Pixel)  {
-        val limitX = pixels(0).length
-        val limitY = pixels.length
+    val inputList : List[String] = new Tools.FileInput(this.inputData).listContent.filterNot(_.trim.isEmpty)
 
-        def getPixel(x : Int, y : Int) : Pixel = {
-            if (y >= limitY || y < 0 || x >= limitX || x < 0)  { pixelOutofImage } else { pixels(y)(x) }
-        }
+    def getPlayers : List[Player] = inputList.map({
+        case s"Player $n starting position: $p" => new Player(n.toInt, p.toInt,0)
+        case _ => new Player(0, 0,0)
+    })
 
-        def affiche() : Unit = {
-            for (i <- 0 until limitY) {
-                for ( j <- 0 until limitX) {
-                    print(this.pixels(i)(j).char)
-                }
-                println
+    val dice : Dice = new Dice(100)
+
+    class Dice(val limit : Int) {
+        var lastValue : Int = 0
+        var rollsNumber : Int = 0
+
+        def rolls( nb : Int ) : Int = {
+            var sumValue : Int = 0
+            for (n <- 1 to nb) {
+                val resTmp = ((lastValue+n)%limit)
+                val tmp = (if (resTmp == 0) (limit) else resTmp)
+                //println(tmp)
+                sumValue += tmp
             }
+            lastValue=(if (lastValue+nb > limit) ((lastValue+nb)%limit) else (lastValue+nb))
+            rollsNumber+=nb
+            //println(sumValue)
+            return sumValue
+        }
+        override def toString : String = s"Dice Rolls [${rollsNumber}] : lastValue [${lastValue}] - Limit [${limit}]"
+    }
+
+    class Player(val number : Int, var position : Int, var score : Int) {
+        var winUniverse : Long = 0
+
+        def move(moveValue : Int): Unit = {
+            val resTmp : Int = (position+moveValue)%10
+            position = (if (resTmp == 0) (10) else (resTmp))
+            score += position
         }
 
-    }
-
-
-    def pixelsToBinary(pixels : IndexedSeq[String]) : Int = Integer.parseInt(pixels.foldLeft("")(_+_),2)
-
-    val dataAlgo : IndexedSeq[Pixel] = inputList(0).toIndexedSeq.map(x => new Pixel(x))
-    val dataInput : Image = new Image((1 until inputList.size).toIndexedSeq.map(x => inputList(x).toIndexedSeq.map(y => new Pixel(y))),new Pixel('.'))
-
-    def getAlgoRes(reference : Image, abs:Int, ord:Int) : Pixel = {
-        val pixels = IndexedSeq((-1,-1),(-1,0),(-1,1),(0,-1),(0,0),(0,1),(1,-1),(1,0),(1,1)).map(x => reference.getPixel(x._2+abs,x._1+ord).getBinary)
-        return dataAlgo(pixelsToBinary(pixels))
-    }
-
-
-    def simuleAlgo(image : Image) : Image = {
-        val newImage : IndexedSeq[IndexedSeq[Pixel]] = {
-            for ( y <- -1 until (image.limitY + 1) )
-                yield
-                    for ( x <- -1 until (image.limitX + 1) )
-                        yield getAlgoRes(image,x,y)
+        def moveWithUniverse(moveValue : Int) : Player = {
+            val playerTmp = new Player(this.number,this.position,this.score)
+            playerTmp.move(moveValue)
+            return playerTmp
         }
-        val pixelOut = if (image.pixelOutofImage.char == '.') dataAlgo(0) else dataAlgo(511)
-        return new Image(newImage,pixelOut)
+
+        override def toString : String = s"Player [${number}] : Pos [${position}] - Score [${score}] - winUniverse [${winUniverse}]"
     }
 
-    def countingChar(image : Image) : Int = {
-        return image.pixels.flatten.groupMapReduce(_.getBinary)(_ => 1)(_+_).filter(x => x._1 == "1").head._2
+
+    @tailrec
+    final def executeGame(score : Int, nbRolls : Int, dice : Dice, players : List[Player]) : (List[Player],Dice) = {
+        if (players.exists(_.score >= score)) {
+            return (players,dice)
+        } else {
+            players.head.move(dice.rolls(nbRolls))
+            //println(players.head)
+            //println(dice)
+            executeGame(score,nbRolls,dice,players.tail:::List(players.head))
+        }
     }
+
+
+    val diceQuantumCombi : List[(Int,Int)] = List(1,2,3).flatMap(x => List(1,2,3).map(y => x+y)).flatMap(x => List(1,2,3).map(y => x+y)).groupMapReduce(identity)(_ => 1)(_+_).toList
+
+    def executeGameQuantum(score : Int, players : (Player,Player), nbrUniverse : Long) : (Long,Long) = {
+        if (players._2.score >= score) {
+            //println(s"${players._2.number} --> Win [${nbrUniverse}]")
+            if (players._2.number == 1) {
+                return (nbrUniverse,0L)
+            } else {
+                return (0L,nbrUniverse)
+            }
+        } else {
+            var res = (0L,0L)
+            for (diceQ <- diceQuantumCombi) {
+                val newNbrUniverse = nbrUniverse * diceQ._2
+                val resTmp = executeGameQuantum(score,(players._2,players._1.moveWithUniverse(diceQ._1)),newNbrUniverse)
+                res = (res._1+resTmp._1 , res._2+resTmp._2)
+            }
+           // println(res)
+            return res
+        }
+    }
+
+
 
 
     def resultExo() : Int = {
-        val res = (1 to 2).foldLeft(dataInput)({case (a,_) => simuleAlgo(a)})
-        return countingChar(res)
+        val (listPlayers,diceResult) = executeGame(1000,3,dice,getPlayers)
+        val looser = listPlayers.filter(_.score < 1000).maxBy(_.score)
+        //println(looser)
+        //println(dice)
+        return (looser.score * dice.rollsNumber)
     }
 
 
-    def resultExoP2() : Int = {
-        val res = (1 to 50).foldLeft(dataInput)({case (a,_) => simuleAlgo(a)})
-        return countingChar(res)
+    def resultExoP2() : Long = {
+        val players = getPlayers
+        val playersResult = executeGameQuantum(21,(players(0),players(1)),1L)
+        return List(playersResult._1,playersResult._2).max
     }
 
 }
 
 
-object Day20 extends App {
-    
-    val data = "data/day20_input.txt"
-    val exo = new Day20(data)
-    
-    println(s"Exercice Day 20 : [${exo.resultExo}]")
-    println(s"Exercice Day 20 Part2 : [${exo.resultExoP2}]")
-    
+object Day21 extends App {
+
+    val data = "data/day21_input.txt"
+    val exo = new Day21(data)
+
+    println(s"Exercice Day 21 : [${exo.resultExo}]")
+    println(s"Exercice Day 21 Part2 : [${exo.resultExoP2}]")
+
 }
 
-//sbt :  ~runMain fr.pragmatias.Exo2021.Day20
+//sbt :  ~runMain fr.pragmatias.Exo2021.Day21
